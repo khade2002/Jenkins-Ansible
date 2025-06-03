dfb9a7964125badfac49f9a4c356c834ded89541
Ubuntu <ubuntu@ip-172-31-12-156.ap-south-1.compute.internal>
2025-06-03 08:36:05 +0000

    Add HTML template for Flask

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__, template_folder="/home/ubuntu/templates")
 
@@ -9,3 +8,4 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
